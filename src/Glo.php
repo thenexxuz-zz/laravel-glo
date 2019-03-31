af13ed9ce2983d80b093d5d2eea97da3ad25accf
William Penton <william@penton.us>
2019-03-30 19:00:19 -0500

    adding options and exceptions

--- src/Glo.php
+++ src/Glo.php
@@ -13,9 +13,47 @@ class Glo
 
     private $clientSecret = null;
 
-    public function __construct()
+    private $perPage = 25;
+
+    private $page = 1;
+
+    public function __construct(array $options)
     {
-        $this->setAccessToken(getenv('GLO_ACCESS_TOKEN'));
+        $accessToken = false;
+        $clientId = false;
+        $clientSecret = false;
+
+        if (array_key_exists('accessToken', $options)) {
+            $this->setAccessToken($options['accessToken']);
+            $accessToken = true;
+        }
+        if (array_key_exists('clientId', $options)) {
+            $this->setClientId($options['clientId']);
+            $clientId = true;
+        }
+        if (array_key_exists('clientSecret', $options)) {
+            $this->setClientSecret($options['clientSecret']);
+        }
+
+        if (array_key_exists('perPage', $options)) {
+            $this->setPerPage($options['perPage']);
+        }
+
+        if (array_key_exists('page', $options)) {
+            $this->setPage($options['page']);
+        }
+
+        if (!$accessToken && !$clientId && $clientSecret) {
+            throw new \Exception('Client ID and Secret both must be set. Client ID is missing.');
+        }
+
+        if (!$accessToken && $clientId && !$clientSecret) {
+            throw new \Exception('Client ID and Secret both must be set. Client secret is missing.');
+        }
+
+        if ($accessToken && $clientId && $clientSecret) {
+            throw new \Exception("Please use only PAT or Client ID/Secret not both.");
+        }
     }
 
     /**
@@ -66,4 +104,35 @@ class Glo
         $this->clientSecret = $clientSecret;
     }
 
+    /**
+     * @return int
+     */
+    public function getPerPage(): int
+    {
+        return $this->perPage;
+    }
+
+    /**
+     * @param int $perPage
+     */
+    public function setPerPage(int $perPage): void
+    {
+        $this->perPage = $perPage;
+    }
+
+    /**
+     * @return int
+     */
+    public function getPage(): int
+    {
+        return $this->page;
+    }
+
+    /**
+     * @param int $page
+     */
+    public function setPage(int $page): void
+    {
+        $this->page = $page;
+    }
 }
