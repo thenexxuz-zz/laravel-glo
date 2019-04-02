80f0b19aea83f87716adfd2394579c0a9d4e089d
William Penton <william@penton.us>
2019-04-02 00:21:21 -0500

    updates
    added Column class
    added updateBoard
    added setColumns
    added addColumn
    added getColumns
    added syncColumns

---
+++ src/Column.php
@@ -0,0 +1,126 @@
+<?php
+
+namespace TheNexxuz\LaravelGlo;
+
+use JsonSerializable;
+
+class Column implements JsonSerializable
+{
+    protected $id = null;
+
+    protected $name = null;
+
+    protected $position = null;
+
+    protected $archivedDate = null;
+
+    protected $createdDate = null;
+
+    protected $createdBy = null;
+
+    public function __construct(string $name = null, $position = null)
+    {
+        $this->setName($name);
+        $this->setPosition($position);
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name): void
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPosition()
+    {
+        return $this->position;
+    }
+
+    /**
+     * @param mixed $position
+     */
+    public function setPosition($position): void
+    {
+        $this->position = $position;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getArchivedDate()
+    {
+        return $this->archivedDate;
+    }
+
+    /**
+     * @param mixed $archivedDate
+     */
+    public function setArchivedDate($archivedDate): void
+    {
+        $this->archivedDate = $archivedDate;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCreatedDate()
+    {
+        return $this->createdDate;
+    }
+
+    /**
+     * @param mixed $createdDate
+     */
+    public function setCreatedDate($createdDate): void
+    {
+        $this->createdDate = $createdDate;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCreatedBy()
+    {
+        return $this->createdBy;
+    }
+
+    /**
+     * @param mixed $createdBy
+     */
+    public function setCreatedBy($createdBy): void
+    {
+        $this->createdBy = $createdBy;
+    }
+    public function jsonSerialize()
+    {
+        return get_object_vars($this);
+    }
+}
--- src/Glo.php
+++ src/Glo.php
@@ -5,6 +5,7 @@ namespace TheNexxuz\LaravelGlo;
 use \StdClass;
 use GuzzleHttp\Client;
 use \Exception;
+use TheNexxuz\LaravelGlo\Column;
 
 class Glo
 {
@@ -13,6 +14,8 @@ class Glo
     private $perPage = 25;
 
     private $page = 1;
+
+    private $columns = [];
 
     public function __construct(array $options)
     {
@@ -28,8 +31,12 @@ class Glo
             $this->setPage($options['page']);
         }
 
+        if (array_key_exists('columns', $options)) {
+            $this->setColumns($options['columns']);
+        }
+
         if (!$this->getAccessToken()) {
-            throw new \Exception("Personal Access Token must be set.");
+            throw new Exception("Personal Access Token must be set.");
         }
     }
 
@@ -95,7 +102,7 @@ class Glo
         $r = new StdClass();
         try {
             $client = new Client();
-            $response = $client->get('https://gloapi.gitkraken.com/v1/glo/boards'  , [
+            $response = $client->get('https://gloapi.gitkraken.com/v1/glo/boards', [
                 'headers' => $this->getHeaders(),
                 'http_errors' => false,
             ]);
@@ -116,7 +123,7 @@ class Glo
         $r = new StdClass();
         try {
             $client = new Client();
-            $response = $client->get('https://gloapi.gitkraken.com/v1/glo/boards/' . $boardId  , [
+            $response = $client->get('https://gloapi.gitkraken.com/v1/glo/boards/' . $boardId . '?fields[]=archived_columns&fields[]=archived_date&fields[]=columns&fields[]=created_by&fields[]=created_date&fields[]=invited_members&fields[]=labels&fields[]=members&fields[]=name', [
                 'headers' => $this->getHeaders(),
                 'http_errors' => false,
             ]);
@@ -129,6 +136,27 @@ class Glo
             ];
             $r->statusCode = 500;
         }
+        $decoded = json_decode($r->data);
+        if (property_exists($decoded, 'columns')) {
+            $cols = [];
+            if (!empty($decoded->columns)) {
+                foreach ($decoded->columns as $c) {
+                    $col = new Column();
+                    $col->setId($c->id);
+                    $col->setName($c->name);
+                    $col->setCreatedBy($c->created_by);
+                    $col->setCreatedDate($c->created_date);
+                    if (property_exists($c, 'position')) {
+                        $col->setPosition($c->position);
+                    }
+                    if (property_exists($c, 'archived_date')) {
+                        $col->setArchivedDate($c->archived_date);
+                    }
+                    $cols[] = $col;
+                }
+            }
+            $this->setColumns($cols);
+        }
         return $r;
     }
 
@@ -137,7 +165,7 @@ class Glo
         $r = new StdClass();
         try {
             $client = new Client();
-            $response = $client->post('https://gloapi.gitkraken.com/v1/glo/boards'  , [
+            $response = $client->post('https://gloapi.gitkraken.com/v1/glo/boards', [
                 'headers' => $this->getHeaders(),
                 'http_errors' => false,
                 'json' => [
@@ -161,12 +189,94 @@ class Glo
         $r = new StdClass();
         try {
             $client = new Client();
-            $response = $client->delete('https://gloapi.gitkraken.com/v1/glo/boards/' . $boardId  , [
+            $response = $client->delete('https://gloapi.gitkraken.com/v1/glo/boards/' . $boardId, [
                 'headers' => $this->getHeaders(),
                 'http_errors' => false,
             ]);
             $r->data = $response->getBody()->getContents();
             $r->statusCode = $response->getStatusCode();
+        }
+        catch (Exception $e) {
+            $r->data = [
+                'error' => $e->getMessage()
+            ];
+            $r->statusCode = 500;
+        }
+        return $r;
+    }
+
+    public function updateBoard(string $boardId, string $name): StdClass
+    {
+        $r = new StdClass();
+        try {
+            $client = new Client();
+            $response = $client->post('https://gloapi.gitkraken.com/v1/glo/boards/' . $boardId, [
+                'headers' => $this->getHeaders(),
+                'http_errors' => false,
+                'json' => [
+                    'name' => $name
+                ],
+            ]);
+            $r->data = $response->getBody()->getContents();
+            $r->statusCode = $response->getStatusCode();
+        }
+        catch (Exception $e) {
+            $r->data = [
+                'error' => $e->getMessage()
+            ];
+            $r->statusCode = 500;
+        }
+        return $r;
+    }
+
+    public function setColumns(array $columns): void
+    {
+        $this->columns = [];
+        if (!empty($columns)) {
+            foreach ($columns as $column) {
+                $this->addColumn($column);
+            }
+        }
+    }
+
+    public function addColumn(Column $column): void
+    {
+        $this->columns[] = $column;
+    }
+
+    public function getColumns(): array
+    {
+        return $this->columns;
+    }
+
+    public function syncColumns(string $boardId): StdClass
+    {
+        $r = new StdClass();
+        $r->data = array();
+        $r->statusCode = 200;
+        try {
+            $client = new Client();
+            if (!empty($this->getColumns())) {
+                foreach ($this->getColumns() as $column) {
+                    $res = new StdClass();
+                    $data = array();
+                    $data['name'] = $column->getName();
+                    if (!is_null($column->getPosition())) {
+                        $data['position'] = $column->getPosition();
+                    }
+                    $response = $client->post('https://gloapi.gitkraken.com/v1/glo/boards/' . $boardId . '/columns', [
+                        'headers' => $this->getHeaders(),
+                        'http_errors' => false,
+                        'json' => $data,
+                    ]);
+                    $res->data = json_decode($response->getBody()->getContents());
+                    $res->statusCode = $response->getStatusCode();
+                    $r->data[] = $res;
+                }
+            }
+            if (!empty($r->data)) {
+                $this->getBoard($boardId);
+            }
         }
         catch (Exception $e) {
             $r->data = [
